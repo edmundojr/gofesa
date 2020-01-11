b1377be48f8013bbf5b24b3e6fa572b932f8bdb5
Edmundo Santos <esantos@poppulo.com>
2020-01-11 15:19:16 +0000

    Updates

---
+++ precache-manifest.296ffa5709c1fa872b584aab23aeaf42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b53fca57329cb04901d5c0276f802e",
+    "url": "/gofesa/index.html"
+  },
+  {
+    "revision": "a83a5412650f030ed7dc",
+    "url": "/gofesa/static/js/2.308dff32.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/gofesa/static/js/2.308dff32.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d117878cd31f0731606",
+    "url": "/gofesa/static/js/main.3ece4f1f.chunk.js"
+  },
+  {
+    "revision": "ccb1b8b713febf748c8b",
+    "url": "/gofesa/static/js/runtime-main.7ec6b1ff.js"
+  }
+]);
